dd9e2ee5d49c020d8008017452519835df62c72d
Thaddeus Brown <thaddeusmbrown@gmail.com>
2022-06-09 19:27:55 -0500

    Add start game dialog, fix display bugs with piece placement, start logic for selecting pieces and identifying valid moves.

--- lib/chess.rb
+++ lib/chess.rb
@@ -5,24 +5,97 @@ require 'pry-byebug'
 class Chess
   def initialize
     @board = Board.new
-    @turns = [@white, @black]
-    @turn = 0
+    @turns = ['White', 'Black']
+    @turn = 1
+    @rank = '12345678'
+    @file = 'abcdefgh'
+    puts 'Welcome to Chess!'
     # ask_ai() implement this function last, optional
     next_turn
   end
 
-  def next_turn
+  protected
 
+  def next_turn
+    @turn = (@turn * -1) + 1
+    player_input
+  end
+
+  def player_input
+    puts "\n#{@turns[@turn]}, please enter a move.  For help, type 'help'."
+    answer = gets.chop
+    if answer == 'help'
+      help
+    else
+      check_valid_input(answer)
+    end
+  end
+
+  def help
+    puts "\nTo move a piece, enter its starting location and ending location.\n
+For example, 'b1 c2' would move a piece from 'b1' to 'c2'.\n
+Castling:  If your king and rooks have not moved and there is space between them, \
+you can castle by moving your king two spaces to the left or right.  The rook will \
+move one space toward the center from where your king lands.\n
+Promotion:  Move a pawn to your opponent's side of the board to promote it to any piece, \
+except a king.\n\n
+En passant:  If an opponent's pawn moves forward two spaces and lands adjacent to your pawn, \
+you can move your pawn diagonally behind it to capture.  This must be done immediately after.\n"
+    player_input
+  end
+
+  def check_valid_input(answer)
+    begin
+      file_start, rank_start, file_end, rank_end = answer[0], answer[1], answer[3], answer[4]
+    rescue
+      invalid_input
+    end
+    if @file.include?(file_start) && @file.include?(file_end) && @rank.include?(rank_start) && @rank.include?(rank_end)
+      check_valid_move(file_start, rank_start.to_i, file_end, rank_end.to_i)
+    else
+      invalid_input
+    end
+  end
+
+  def check_valid_move(file_start, rank_start, file_end, rank_end)
+    file_start, rank_start, file_end, rank_end = file_start.ord - 97, rank_start - 1, file_end.ord - 97, rank_end - 1
+    if @turn.zero?
+      @board.white_player.pieces.each do |piece|
+        set_piece(piece, @board.white_player.locations, @board.black_player.locations) if piece.location == [rank_start, file_start]
+      end
+    else
+      @board.black_player.pieces.each do |piece|
+        set_piece(piece, @board.black_player.locations, @board.white_player.locations) if piece.location == [rank_start, file_start]
+      end
+    end
+    no_owned_piece
+  end
+
+  def set_piece(piece, friendly_locations, enemy_locations)
+    valid_moves = piece.find_valid_moves(friendly_locations, enemy_locations)
+  end
+
+  def invalid_input
+    puts "Invalid input, try again."
+    player_input
+  end
+
+  def no_owned_piece
+    puts "No owned piece at starting location, try again."
+    player_input
   end
 end
 
 # creates the board, stores piece locations, and displays
 # @board is a 2-D array, outer level (i: 0-7) is a-g, inner level (j: 0-7) is 1-8
 class Board
+  attr_reader :white_player, :black_player
+
   def initialize
     @white_player = Player.new('white')
     @black_player = Player.new('black')
     @board = Array.new(8) { Array.new(8, nil) }
+    build_board
     @black = '  '.on_magenta
     @white = '  '.on_light_magenta
     @odd_row = [@black, @white, @black, @white, @black, @white, @black, @white]
@@ -30,43 +103,47 @@ class Board
     display_board
   end
 
+  protected
+
   def display_board
     @board.reverse.each_with_index do |row, index|
       row_type = index.even? ? Array.new(@even_row) : Array.new(@odd_row)
       reverse_index = 7 - index
-      @white_player.pieces.each do |piece|
-        if piece.location[0] == reverse_index
-          if (piece.location[0] + piece.location[1]).odd?
-            row_type[piece.location[1]] = piece.icon.white.on_light_magenta + ' '.on_light_magenta
-          else
-            row_type[piece.location[1]] = piece.icon.white.on_magenta + ' '.on_magenta
-          end
+      @board[index].each_with_index do |piece, col|
+        unless piece.nil?
+          row_type[col] = piece.location.sum.even? ? piece.icon.on_magenta + ' '.on_magenta : piece.icon.on_light_magenta + ' '.on_light_magenta 
         end
       end
-      @black_player.pieces.each do |piece|
-       # binding.pry if reverse_index == 5
-        if piece.location[0] == reverse_index
-          if (piece.location[0] + piece.location[1]).odd?
-            row_type[piece.location[1]] = piece.icon.black.on_light_magenta + ' '.on_light_magenta
-          else
-            row_type[piece.location[1]] = piece.icon.black.on_magenta + ' '.on_magenta
-          end
-        end
-      end
-      puts reverse_index.to_s + ' ' + row_type.join
+      puts (reverse_index + 1).to_s + ' ' + row_type.join
     end
     puts '  a b c d e f g h'
+  end
+
+  def build_board
+    @board.each_with_index do |row, index|
+      reverse_index = 7 - index
+      @white_player.pieces.each do |piece|
+        row[piece.location[1]] = piece if piece.location[0] == reverse_index
+      end
+      @black_player.pieces.each do |piece|
+        row[piece.location[1]] = piece if piece.location[0] == reverse_index
+      end
+    end
   end
 end
 
 # one white and one black player, stores and controls pieces
 class Player
-  attr_reader :pieces
+  attr_reader :pieces, :side, :locations
 
   def initialize(color)
     @side = color
     @pieces = create_pieces
+    @locations = []
+    refresh_locations
   end
+
+  protected
 
   def create_pieces
     pieces = []
@@ -91,15 +168,23 @@ class Player
       King.new(col, side)
     end
   end
+
+  def refresh_locations
+    @locations = []
+    @pieces.each do |piece|
+      @locations.push piece.location
+    end
+  end
 end
 
 # base class for shared properties of all pieces
 class Piece
-  attr_reader :side, :location, :icon
+  attr_reader :side, :location, :icon, :valid_moves
 
   def initialize(col, side)
     @side = side
     @location = side == 'white' ? [0, col] : [7, col]
+    @valid_moves = []
   end
 end
 
@@ -108,7 +193,20 @@ class Pawn < Piece
   def initialize(col, side)
     super
     @location = side == 'white' ? [1, col] : [6, col]
-    @icon = "\u265f".encode('utf-8')
+    @icon = side == 'white' ? "\u265f".encode('utf-8').white : "\u265f".encode('utf-8').black
+    @move_bit = false
+    @potential_moves = [[1, 0], [2, 0], [1, -1], [1, 1]]
+  end
+
+  def find_valid_moves(friendly_locations, enemy_locations)
+    @valid_moves = []
+    check_moves = []
+    @potential_moves.each do |move|
+      check_moves.push(@location + move)
+    end
+
+
+
   end
 end
 
@@ -116,7 +214,7 @@ end
 class Knight < Piece
   def initialize(col, side)
     super
-    @icon = "\u265e".encode('utf-8')
+    @icon = side == 'white' ? "\u265e".encode('utf-8').white : "\u265e".encode('utf-8').black
   end
 end
 
@@ -124,7 +222,7 @@ end
 class Bishop < Piece
   def initialize(col, side)
     super
-    @icon = "\u265d".encode('utf-8')
+    @icon = side == 'white' ? "\u265d".encode('utf-8').white : "\u265d".encode('utf-8').black
   end
 end
 
@@ -132,7 +230,7 @@ end
 class Rook < Piece
   def initialize(col, side)
     super
-    @icon = "\u265c".encode('utf-8')
+    @icon = side == 'white' ? "\u265c".encode('utf-8').white : "\u265c".encode('utf-8').black
   end
 end
 
@@ -140,7 +238,7 @@ end
 class Queen < Piece
   def initialize(col, side)
     super
-    @icon = "\u265b".encode('utf-8')
+    @icon = side == 'white' ? "\u265b".encode('utf-8').white : "\u265b".encode('utf-8').black
   end
 end
 
@@ -148,7 +246,7 @@ end
 class King < Piece
   def initialize(col, side)
     super
-    @icon = "\u265a".encode('utf-8')
+    @icon = side == 'white' ? "\u265a".encode('utf-8').white : "\u265a".encode('utf-8').black
   end
 end
-Board.new
+Chess.new
